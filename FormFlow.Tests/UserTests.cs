264703a9a70a26fd6fba799643b9da3bf8da73be
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-06-08 23:55:09 +0200

    Add check database test

--- FormFlow.Tests/UserTests.cs
+++ FormFlow.Tests/UserTests.cs
@@ -12,35 +12,39 @@ using Microsoft.AspNetCore.Mvc;
 using FormFlow.Data.Repositories;
 using FormFlow.JWT;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace FormFlow.Tests
 {
 	public class UserTests
 	{
-		[Fact]
-		public async Task RegisterUser_ValidUser_ReturnsCreatedResult()
-		{
-			// Arrange
-			var userRepositoryMock = new Mock<UserRepository>();
-			var formRepositoryMock = new Mock<FormRepository>();
-			var jwtSettingsMock = new Mock<JwtSettings>();
-			var userController = new UserController(userRepositoryMock.Object, formRepositoryMock.Object, jwtSettingsMock.Object);
+		private Mock<IOptions<MongoDBSettings>> _mockOptions;
+		private Mock<IMongoDatabase> _mockDB;
+		private Mock<IMongoClient> _mockClient;
 
-			var newUser = new UserRegistrationModel
+		public UserTests()
+		{
+			_mockOptions = new Mock<IOptions<MongoDBSettings>>();
+			_mockDB = new Mock<IMongoDatabase>();
+			_mockClient = new Mock<IMongoClient>();
+		}
+		[Fact]
+		public void ApplicationDBContext_Constructor_Success()
+		{
+			var settings = new MongoDBSettings
 			{
-				Email = "test@example.com",
-				Password = "examplePassword"
+				ConnectionURI = "mongodb://tes123 ",
+				DatabaseName = "TestDB"
 			};
 
-			// Set up the mock behavior for the userRepositoryMock
-			userRepositoryMock.Setup(u => u.CreateAsync(It.IsAny<User>())).Returns(Task.CompletedTask);
+			_mockOptions.Setup(s => s.Value).Returns(settings);
+			_mockClient.Setup(c => c
+					.GetDatabase(_mockOptions.Object.Value.DatabaseName, null))
+				.Returns(_mockDB.Object);
 
-			// Act
-			var result = await userController.Register(newUser);
+			var context = new MongoDbContext(_mockOptions.Object);
 
-			// Assert
-			Assert.IsType<CreatedAtActionResult>(result);
-			userRepositoryMock.Verify(u => u.CreateAsync(newUser), Times.Once);
+			Assert.NotNull(context);
 		}
 	}
 }
