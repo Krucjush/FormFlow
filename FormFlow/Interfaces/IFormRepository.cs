b65fdcc4e5a2c3f82e356d93e7f5be546ef65997
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-06-09 21:10:01 +0200

    Add registration tests

---
+++ FormFlow/Interfaces/IFormRepository.cs
@@ -0,0 +1,15 @@
+﻿using FormFlow.Models;
+
+namespace FormFlow.Interfaces
+{
+	public interface IFormRepository
+	{
+		public Task CreateAsync(Form form);
+		public Task<List<Form>> GetAsync();
+		public Task<Form> GetByIdAsync(string id);
+		public Task UpdateAsync(Form form);
+		public Task DeleteAsync(string id);
+		public Task<bool> HasResponsesAsync(string formId);
+		public Task DisassociateFormsFromUserAsync(string userId);
+	}
+}
