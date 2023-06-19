925fdce4bef5440fbd58d24c3ff561c87fbc6745
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-06-20 01:03:30 +0200

    Fix form creation

---
+++ FormFlow/Controllers/QuestionController.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using FormFlow.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormFlow.Controllers
+{
+	public class QuestionController : Controller
+	{
+		[HttpGet]
+		public IActionResult GetQuestionTypes()
+		{
+			var questionTypes = Enum.GetNames(typeof(QuestionType)).ToList();
+			return Json(questionTypes);
+		}
+	}
+}
