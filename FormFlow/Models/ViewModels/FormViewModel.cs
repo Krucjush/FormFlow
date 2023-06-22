a5b8c2206560745073c7493ca6968905fb2812f9
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-06-22 18:27:26 +0200

    Add from modification view

--- FormFlow/Models/ViewModels/FormViewModel.cs
+++ FormFlow/Models/ViewModels/FormViewModel.cs
@@ -6,7 +6,7 @@ namespace FormFlow.Models.ViewModels
 	public class FormViewModel
 	{
 		public IEnumerable<Form> ListForms { get; set; }
-		public Form Form { get; set; }
+		public Form? Form { get; set; }
         public Question Question { get; set; }
 		[Required]
         public List<Question> Questions { get; set; }
